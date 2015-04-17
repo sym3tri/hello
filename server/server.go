8b26da3124bd24d6ef8c2dbe3c1e48cc39804bc5
Ed Rooth <ed.rooth@coreos.com>
2015-04-17 16:42:58 -0700

    some additional handlers for testing

--- server/server.go
+++ server/server.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
 )
 
 const version = "v2.0.0"
@@ -18,16 +21,73 @@ type Server struct {
 func (s *Server) HTTPHandler() http.Handler {
 	mux := http.NewServeMux()
 	mux.Handle("/", s.rootHandler())
+	mux.Handle("/version", s.versionHandler())
+	mux.Handle("/prestop", s.prestopHandler())
+	mux.Handle("/poststart", s.poststartHandler())
+	mux.Handle("/mount/", s.mountHandler())
 	return http.Handler(mux)
-}
-
-func (s *Server) rootHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello %s.\n", s.Config.Message)
-		fmt.Fprintf(w, "Version: %s\n", version)
-	}
 }
 
 func (s *Server) Version() string {
 	return version
+}
+
+func (s *Server) rootHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, message: %s\n", s.Config.Message)
+	}
+}
+
+func (s *Server) versionHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Version: %s\n", version)
+	}
+}
+
+func (s *Server) poststartHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("post-start")
+		fmt.Fprint(w, "post-start")
+	}
+}
+
+func (s *Server) prestopHandler() http.HandlerFunc {
+	waitSecs := 5
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "initiated pre-stop\n")
+		log.Println("initiated pre-stop")
+		for i := waitSecs; i > 0; i-- {
+			log.Printf("shutting down in: %ds", i)
+			fmt.Fprintf(w, "shutting down in: %ds\n", i)
+			time.Sleep(time.Second * 1)
+		}
+		log.Println("pre-stop complete")
+		fmt.Fprint(w, "pre-stop complete")
+	}
+}
+
+// mountHandler Prints the contents of the file specified in the 'file' query param.
+func (s *Server) mountHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		q := r.URL.Query()
+		filename := q.Get("file")
+		if filename == "" {
+			fmt.Fprintf(w, "No file param specified")
+			return
+		}
+
+		b, err := ioutil.ReadFile(filename)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "file not mounted: %s\n", filename)
+			return
+		}
+		fmt.Fprintf(w, "file contents for: %s\n", filename)
+		w.Write(b)
+	}
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "not found")
 }
